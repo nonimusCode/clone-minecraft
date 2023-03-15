e78dac46482722d0d7cea343be205445937fbf5e
nonimusj <nonimus.code@gmail.com>
2023-03-15 01:24:32 -0500

    The player component is created to simulate a player in the third person, the payer is left advancing forward, the component is also placed in the app.js to render and be affected by the physics

---
+++ src/components/Player.jsx
@@ -0,0 +1,43 @@
+import { useSphere } from '@react-three/cannon'
+import { useFrame, useThree } from '@react-three/fiber'
+import { useEffect, useRef } from 'react'
+import { Vector3 } from 'three'
+
+export function Player() {
+    const { camera } = useThree()
+    const [ref, api] = useSphere(() => ({
+        mass: 1,
+        type: 'Dynamic',
+        position: [0, 1, 0]
+    }))
+    const pos = useRef([0, 0, 0])
+
+    useEffect(() => {
+        api.position.subscribe(p => {
+            pos.current = p
+        })
+    }, [api.position])
+
+    const vel = useRef([0, 0, 0])
+
+    useEffect(() => {
+        api.velocity.subscribe(v => {
+            vel.current = v
+        })
+    }, [api.velocity])
+
+    useFrame(() => {
+        camera.position.copy(
+            new Vector3(
+                pos.current[0],
+                pos.current[1],
+                pos.current[2],
+            )
+        )
+        api.velocity.set(0, 0, -0.5)
+            })
+
+    return (
+      <mesh ref={ref} />
+    )
+}
